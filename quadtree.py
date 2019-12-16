dea10eb68bc4f2554e6dfe4001a7af88f9752469
zuravskm <marketa.zuravska@seznam.cz>
2019-12-16 15:11:21 +0100

    try to push it on GitHub

---
+++ quadtree.py
@@ -0,0 +1,113 @@
+### order coordinates by axis
+# input = list points
+# output = sorting data
+
+
+def find_max_min(points, axis):
+    points.sort(key=lambda p: ["geometry"]["coordinates"][axis])
+    return points
+
+
+### compute length of bounding box
+# it probably won't be needed
+# maybe for turtle drawing
+
+
+def bbox_len_x (points_sort_x):
+    x_max = max(points_sort_x)
+    x_min = min(points_sort_x)
+    length_bbox_x = x_max[0] - x_min[0]
+    # print(x_max, x_min)
+    # print("x", length_bbox_x)
+    return length_bbox_x
+
+
+def bbox_len_y(points_sort_y):
+    y_max = max(points_sort_y)
+    y_min = min(points_sort_y)
+    length_bbox_y = y_min[1] - y_max[1]
+    # print(y_max, y_min)
+    # print("y", length_bbox_y)
+    return length_bbox_y
+
+
+### find min a max values of coordinates
+# input = points
+# output = end points of bounding box
+
+
+def bbox(points):
+    bbox = []
+    points_sort_x = find_max_min(points, "x")
+    x_min = points_sort_x[0][1]
+    x_max = points_sort_x[-1][1]
+    bbox.append(x_max)
+    bbox.append(x_min)
+    points_sort_y = find_max_min(points, "y")
+    y_max = points_sort_y[0][2]
+    y_min = points_sort_y[-1][2]
+    bbox.append(y_max)
+    bbox.append(y_min)
+    return bbox
+
+
+### select points in the new quadrant and creating a unique group identifier for the new quadrant (cluster_id)
+# quad_box = boundaries of new quadrants
+# quad_num = numbers of new quadrants
+
+def select_new_quad_poi(points_in, quad_box, quadrant):
+    new_poi = []
+    for poi in points_in:
+        ID = poi[0]
+        coordx = poi[1]
+        coordy = poi[2]
+        old_cluster_id = poi[3]
+        cluster_id = old_cluster_id + "1"
+        if quad_box[1] <= coordx <= quad_box[0] and quad_box[3] <= coordy <= quad_box[2]:
+            new_poi.append([ID, coordx, coordy, cluster_id])
+            return new_poi
+        else:
+            continue
+
+
+### distribution data by quadtree
+# num_poi = the minimum number of points in the quadrant
+# the first time using this function points_in = points
+# the first time using this function quadrant = 0
+# structure of bbox_end_poi = [x_max, x_min, y_max, y_min]
+
+
+def quadtree_build(points_in, points_out, bbox_end_poi, quadrant, num_poi):
+    if len(points_in) <= num_poi:
+        for poi in points_in:
+            points_out.append(poi)
+        return points_out
+    else:
+        center_box = [(bbox_end_poi[1]+bbox_end_poi[0])/2, (bbox_end_poi[3]+bbox_end_poi[2])/2]
+        quad_1_bbox = [center_box[0], bbox_end_poi[1], bbox_end_poi[2], center_box[1]]
+        quad_2_bbox = [bbox_end_poi[0], center_box[0], bbox_end_poi[2], center_box[1]]
+        quad_3_bbox = [center_box[0], bbox_end_poi[1], center_box[1], bbox_end_poi[3]]
+        quad_4_bbox = [bbox_end_poi[0], center_box[0], center_box[1], bbox_end_poi[3]]
+
+        # counts points in the newly created quadrate and saves them in a new list
+        new_poi_1 = select_new_quad_poi(points_in, quad_1_bbox, 1)
+        new_poi_2 = select_new_quad_poi(points_in, quad_2_bbox, 2)
+        new_poi_3 = select_new_quad_poi(points_in, quad_3_bbox, 3)
+        new_poi_4 = select_new_quad_poi(points_in, quad_4_bbox, 4)
+
+        # recursive function call
+        quadtree_build(new_poi_1, points_out, quad_1_bbox, 1, num_poi)
+        quadtree_build(new_poi_2, points_out, quad_2_bbox, 2, num_poi)
+        quadtree_build(new_poi_3, points_out, quad_3_bbox, 3, num_poi)
+        quadtree_build(new_poi_4, points_out, quad_4_bbox, 4, num_poi)
+    return points_out
+
+
+### finally saving the output to a new GoeJSON file
+
+
+def create_new_geojson (points_out):
+    gj_structure = {'type': 'FeatureCollection'}
+    # gj_structure['features'] = something
+    with open("output.geojson", "w", encoding="utf-8") as f:
+        points_out.dump(gj_structure, f, indent=2, ensure_ascii=False)
---
+++ split.py
@@ -0,0 +1,52 @@
+import json
+import quadtree
+
+### loading input GeoJSON data
+
+with open("input.geojson", "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+
+### creating new list od data from GeoJson
+
+points = data["features"]
+
+
+### order coordinates by axis
+# input = list points
+# output = sorting data = list of points_sort_x, points_sort_y
+
+
+points_sort_x = quadtree.find_max_min(points, 0)
+print(points_sort_x)
+points_sort_y = quadtree.find_max_min(points, 1)
+print(points_sort_y)
+
+
+### find min a max values of coordinates
+# input = points
+# output = end points of bounding box
+# structure of bbox_end_poi = [x_max, x_min, y_max, y_min]
+
+
+bbox_end_poi = quadtree.bbox(points)
+print(bbox_end_poi)
+
+
+### distribution data by quadtree and finally saving the output to a new GoeJSON file
+# num_poi = the minimum number of points in the quadrant
+
+
+quadrant = 0
+num_poi = 50 # minimum number of points in quadrants
+
+
+if len(points) <= num_poi:
+    points_out = points.append([ID, coord[0], coord[1], 0]) # add cluster_id 0
+    quadtree.create_new_geojson(points_out)
+
+else:
+    points_out = []
+    new_points = quadtree.select_new_quad_poi(points, bbox_end_poi, quadrant)
+    points_out = quadtree.quadtree_build(new_points, points_out, bbox_end_poi, quadrant, num_poi)
+    quadtree.create_new_geojson(points_out)
